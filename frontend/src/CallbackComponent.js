af49213c206fb717922646d07b49c26ed4521624
Ahmad Alsharef <ahmadalsharef2021@gmail.com>
2024-01-17 09:16:57 +0200

    *

---
+++ frontend/src/CallbackComponent.js
@@ -0,0 +1,21 @@
+// import React, { useEffect } from 'react';
+
+// const CallbackComponent = ({ onTokenReceived }) => {
+//   useEffect(() => {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const code = urlParams.get('code');
+
+//     if (code) {
+//       // Call the function passed as a prop to handle the token exchange
+//       onTokenReceived(code);
+//     }
+//   }, [onTokenReceived]);
+
+//   return (
+//     <div>
+//       Processing login...
+//     </div>
+//   );
+// };
+
+// export default CallbackComponent;
